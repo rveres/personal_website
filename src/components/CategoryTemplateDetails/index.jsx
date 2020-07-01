94dcfefb5e44cd40396c6829dcdafde2df5b2e2d
Robert Veres <thedeveloper733@gmail.com>
2020-07-01 17:00:43 -0400

    Fixing GraphQL error

--- src/components/CategoryTemplateDetails/index.jsx
+++ src/components/CategoryTemplateDetails/index.jsx
@@ -15,7 +15,7 @@ class CategoryTemplateDetails extends React.Component {
       <div className="content">
         <div className="content__inner">
           <div className="page">
-            <h1 className="page__title">{category}</h1>
+            <h1 className="page__title">All {category} Posts</h1>
             <div className="page__body">{items}</div>
           </div>
         </div>
